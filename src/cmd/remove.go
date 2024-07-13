7fa480a29b24f5306870482e72abe821b61d6174
Theis <thph@itu.dk>
2024-07-13 11:27:38 +0200

    refactor: change remove command to be less dangerous now only uses remove to be safer on config directory

--- src/cmd/remove.go
+++ src/cmd/remove.go
@@ -22,11 +22,22 @@ var removeCmd = &cobra.Command{
 			os.Exit(0)
 		}
 
-		err = os.RemoveAll(UserConf())
+		err = os.Remove(UserConf()+"config.json")
 		if err != nil {
 			fmt.Println("Failed to remove the configuration file")
 			fmt.Println(err)
+			os.Exit(1)
 		}
+
+
+		err = os.Remove(UserConf())
+		if err != nil {
+			fmt.Println("Failed to remove the configuration folder")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Configuration file removed successfully")
 	},
 }
 
