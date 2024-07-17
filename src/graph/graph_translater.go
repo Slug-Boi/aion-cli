d8cb3f3bd248b7795928c8df2101f8abd35f585c
Theis <thph@itu.dk>
2024-07-17 13:35:45 +0200

    refactor: remove debug print and unused function

--- src/graph/graph_translater.go
+++ src/graph/graph_translater.go
@@ -1,9 +1,7 @@
 package graph
 
 import (
-	"math/rand"
 	"strings"
-	"time"
 
 	"github.com/Slug-Boi/aion-cli/forms"
 )
@@ -60,9 +58,7 @@ func Translate(data forms.Form) ([]Edge, int, map[int]User) {
 			} else {
 				cap = 1.0
 			}
-			if cap == cap+floats[i] {
-				println("yes")
-			}
+
 			graph = append(graph, Edge{From: userNodeInc, To: timeslotNodeInc, Capacity: 1, Cost: cap + floats[i]})
 			timeslotNodeInc++
 		}
@@ -76,17 +72,4 @@ func Translate(data forms.Form) ([]Edge, int, map[int]User) {
 		graph = append(graph, Edge{From: i, To: timeslotNodeInc, Capacity: 1, Cost: 0})
 	}
 	return graph, timeslotNodeInc, nodeToUser
-}
-
-// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randSeq(n int) string {
-	rand.Seed(time.Now().UnixNano())
-
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
 }
