c8a407817ea15d15e870b6bbd5aa5bcac333198b
Theis <thph@itu.dk>
2024-07-26 11:59:08 +0200

    feat: add gurobi solver package with the gurobi solver python file (will be changed to be an embedded file later)

---
+++ src/gurobi/gurobi.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3.11
+
+# Copyright 2024, Gurobi Optimization, LLC
+
+# This code is very heavily based on the gurobi workforce1 python example:
+# https://docs.gurobi.com/projects/examples/en/current/examples/workforce.html#subsectionworkforce1examples
+
+
+# Objective: Assign groups to timeslots;
+# Each group must be assigned to exactly one timeslot
+# Each timeslot must at most have one group assigned to it or none
+# The objective is to minimize the cost of assigning groups to timeslots
+# based on the wishes of the groups
+
+
+# note from gurobi example: if the problem cannot be solved, use IIS to
+# find a set of conflicting constraints. Note that there may
+# be additional conflicts besides what is reported via IIS.
+
+# debug test command:
+# $ python gurobi.py "gr1,gr2" "Tue2:gr1:4,Tue2:gr2:1,Wed1:gr2:1,Wed2:gr1:1"
+
+import gurobipy as gp
+from gurobipy import GRB
+import sys
+
+args = sys.argv
+
+if len(args) != 3:
+    print("Usage: gurobi.py \"<group1>,<group2>..."" \"<timeslot1:group1:cost1>,<timeslot2:group2:cost2>...\"")
+    sys.exit(1)
+
+groups = args[1].split(",")
+timeslots = args[2].split(",")
+
+groupdict = {}
+for group in groups:
+    groupdict[group] = 1
+
+dayGroupTupleList = []
+timeslotCostDict = {}
+
+for timeslot in timeslots:
+    day, group, cost = timeslot.split(";")
+    dayGroupTupleList.append((day, group))
+    # TODO: might need to change to a float but wait
+    timeslotCostDict[(day, group)] = float(cost)
+
+# TODO: Change variable names to fit the problem better
+# Number of groups needed for each timeslot (This will always be 1 as we need
+# a 1-1 relation between groups
+# and timeslots)
+timeslots, timeslotRequirements = gp.multidict(groupdict)
+# append groups to shifts and shiftRequirements
+
+
+# The cost of assigning a group to a timeslot this is calculated from
+# the wishes given by the groups
+# in the google form
+# Green = 0, Yellow = 10/sum, red = 100/sum
+# All values have a heuristic tie breaker value added to them
+cost = gp.tupledict(timeslotCostDict)
+
+# Mapping from timeslot to group this is what we need to minimize the cost of
+availability = gp.tuplelist(dayGroupTupleList)
+
+# Model
+m = gp.Model("assignment")
+
+# Assignment variables: x[w,s] == 1 if worker w is assigned to shift s.
+# Since an assignment model always produces integer solutions, we use
+# continuous variables and solve as an LP.
+x = m.addVars(availability, ub=1, name="x")
+
+# The objective is to minimize the total pay costs
+m.setObjective(gp.quicksum(cost[w, s] * x[w, s]
+               for w, s in availability), GRB.MINIMIZE)
+
+
+# Constraints: assign exactly shiftRequirements[s] workers to each shift s
+reqCts = m.addConstrs(
+    (x.sum("*", s) == timeslotRequirements[s] for s in timeslots), "_")
+
+# Constraints: each group is assigned at most one timeslot
+m.addConstrs(x.sum(s, "*") <= 1 for s, _ in availability)
+
+
+# Save model
+m.write("aion.lp")
+
+
+# TODO: Figure out how to get the relevant data out of the solver afterwards
+# variables we want:
+# Path from group to timeslot (e.g gr1 -> Tue2)
+# Cost of the assignment (e.g. gr1 -> Tue2 = 4)
+# The total cost of the Assignment (e.g. 4 + 1 + 1 + 1)
+# Solve time (e.g. 0.01s)
+
+# Optimize
+m.optimize()
+status = m.Status
+if status == GRB.UNBOUNDED:
+    print("The model cannot be solved because it is unbounded")
+    sys.exit(0)
+if status == GRB.OPTIMAL:
+    print(f"The optimal objective is {m.ObjVal:g}")
+    sys.exit(0)
+if status != GRB.INF_OR_UNBD and status != GRB.INFEASIBLE:
+    print(f"Optimization was stopped with status {status}")
+    sys.exit(0)
+
+# do IIS
+print("The model is infeasible; computing IIS")
+m.computeIIS()
+if m.IISMinimal:
+    print("IIS is minimal\n")
+else:
+    print("IIS is not minimal\n")
+print("\nThe following constraint(s) cannot be satisfied:")
+for c in m.getConstrs():
+    if c.IISConstr:
+        print(c.ConstrName)
---
+++ src/gurobi/gurobi_translator.go
@@ -0,0 +1,83 @@
+package gurobi
+
+import (
+	"fmt"
+	"os/exec"
+	"sort"
+	"strings"
+
+	"github.com/Slug-Boi/aion-cli/forms"
+	"github.com/Slug-Boi/aion-cli/graph"
+)
+
+// Translates the form data to Gurobi syntax (this is proprietary to how the gurobi python program works)
+// a new translator will more than likely need to be created for any other optimization program
+func TranslateGurobi(data []forms.Form) (string, string) {
+
+	// Cache is used to store the group name going to the ID for consistent hashing later
+	cache := map[string]string{}
+
+	// Sort users by id to ensure consistent ordering when generating the concatenated string
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].HashString < data[j].HashString
+	})
+
+	// Create base string for creating heuristic
+	sb := strings.Builder{}
+
+	// Get the string of all group inputs and the cache
+	allStrings, cache := graph.BaseHashString(data, cache, sb)
+
+	// Create string builders for two return strings
+	sbGroups := strings.Builder{}
+	sbTimeslots := strings.Builder{}
+
+	for _, participant := range data {
+		// Add group to string builder
+		sbGroups.WriteString(participant.GroupNumber + ",")
+
+		// Create heuristic for the participant
+		heuristic := graph.HashHeuristic(cache[participant.GroupNumber], allStrings)
+
+		// Translate timeslots to participant linked nodes:
+		// Caps are all the individual costs for each timeslot
+		// SumCap is the sum of all the costs for each timeslot
+		caps :=  map[string]float64{}
+		var sumCap float64
+
+		// Calculation is done this way to make sure that Want are all weighted equally
+		// Can do and Cannot are weighted different between groups with the idea being that groups
+		// with many wishes will get lower values overall on their wishes meaning they are more likely to get
+		// their wishes granted as they are more flexible. Can do will always be weighted lower than cannot
+		// due to the sum division. These calculations give a more fair distribution of timeslots between groups
+		// and will incentivize groups to be more flexible with their timeslots and answer truthfully
+		for timeslot, vote := range participant.Votes {
+			caps, sumCap = graph.CostSummer(timeslot,vote, caps, sumCap)
+		}
+
+		// add the timeslot costs to the string builder
+		for timeslot, cap := range caps {
+			sbTimeslots.WriteString(timeslot+";"+participant.GroupNumber+";"+fmt.Sprintf("%f",(cap/sumCap)+heuristic)+",")
+		}
+	}
+
+	return sbGroups.String()[:sbGroups.Len()-1], sbTimeslots.String()[:sbTimeslots.Len()-1]
+}
+
+// Currently this runs the Gurobi optimization through python. There is a gurobi library for Go
+// but it is different syntax wise so this is a temporary solution until the Go library is implemented
+// (if time permits)
+func RunGurobi(groups, timeslots string) (string, error) {
+	// Run the gurobi python script
+	println("groups:",groups)
+	println("timeslots:",timeslots)
+
+	cmd := exec.Command("python", "./gurobi/gurobi.py", groups, timeslots)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
