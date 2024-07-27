976c3fbe117c0c7cb91941e3f727b54920171f0b
Theis <thph@itu.dk>
2024-07-27 22:42:08 +0200

    feat: add tailwind templates as a first go around for the html templates data is now displayed correctly

--- src/html/html_template.go
+++ src/html/html_template.go
@@ -4,7 +4,19 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"embed"
+
+	"github.com/Slug-Boi/aion-cli/solvers/graph"
 )
+
+//go:embed root.html
+var htmlFiles embed.FS
+
+//go:embed css/*
+var cssFiles embed.FS
 
 type WebData struct {
 	GroupNumber string
@@ -16,42 +28,66 @@ type WebData struct {
 }
 
 // HTML template code inspired by https://gowebexamples.com/templates/
-func GenerateHTML(input []WebData) {
+func GenerateHTML(args []string) {
+	iterations := 0
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		const templ = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="UTF-8">
-          <title>Aion Timeslot Management</title>
-        </head>
-		<body>
-		<h1>Timeslot Management</h1>
-		 <button>Re-Solve</button> 
-		 <button>Advanced View</button>
-		 <button>Download PDF</button>
-        <ol>
-          {{range .WebData}}<li>{{.GroupNumber}} {{.Timeslot}} {{.Day}} {{.Date}} {{.WishLevel}}</li>{{end}}
-        </ol>
-		<script>
-			function downloadPDF() { 
+		var webData []WebData
+		iterations++
+		// Run the solver
+		sink, users, _, paths, nodeToTimeslot := graph.SolveMin_Cost(args)
 
+		finalPaths := map[int]int{}
+
+		// Create a map that maps the group number to the timeslot
+		// This is required since some paths will have residual graphs in them which will overwrite
+		// Previous paths chosen
+		for _, path := range paths {
+			i := sink
+			timeslotNode := -1
+			for i != 0 {
+				if _, ok := users[i]; !ok {
+					timeslotNode = i
+				} else {
+					if timeslotNode != -1 {
+						finalPaths[i] = timeslotNode
+					}
+					timeslotNode = -1
+				}
+				i = path[i]
+			}
 		}
-		</script>
-		</body>
-      </html>`
 
-		t, err := template.New("webpage").Parse(templ)
+		// TODO: Convert path slice to an actual linear path to reduce nested calls as it looks disgusting
+		// Might be as simple as using the uncommented code below try later
+		for user, timeslot := range finalPaths {
+			timeslotStr := nodeToTimeslot[timeslot]
+			Date_Day_Timeslot := strings.Split(timeslotStr, " ")
+			trimmedTimeslot := strings.Trim(Date_Day_Timeslot[2], "[]")
+			day := Date_Day_Timeslot[1]
+			date := Date_Day_Timeslot[0]
+			wishLevel := users[user].Votes[timeslotStr]
+
+			webData = append(webData, WebData{GroupNumber: users[user].GroupNumber, Timeslot: trimmedTimeslot, Day: day, Date: date, WishLevel: wishLevel})
+		}
+
+		// sort by group number
+		sort.Slice(webData, func(i, j int) bool {
+			return webData[i].GroupNumber < webData[j].GroupNumber
+		})
+		
+		t, err := template.ParseFS(htmlFiles, "root.html")
 		if err != nil {
 			//TODO: change to Zap logger later
 			log.Fatal(err)
 		}
 
 		data := struct {
-			WebData []WebData
+			WebData    []WebData
+			Iterations int
 		}{
-			WebData: input,
+			WebData:    webData,
+			Iterations: iterations,
 		}
 
 		err = t.Execute(w, data)
@@ -60,6 +96,16 @@ func GenerateHTML(input []WebData) {
 		}
 	})
 
+	// Change the working directory to the html folder
+	os.Chdir("html")
+
+	// Serve the css file for the html
+	var css http.FileSystem = http.FS(cssFiles)
+	http.Handle("/css/", http.FileServer(css))
+	//fs := http.FileServer(http.Dir("./css/"))
+	//http.Handle("/css/", http.StripPrefix("/css", fs))
+
+	// Launch the server on port 80
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		// TODO: Use zaplogger to log the error.
---
+++ src/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../html/root.html"],
+  theme: {
+    extend: {      
+      fontFamily: {
+      sans: ["Iosevka Aile Iaso", "sans-serif"],
+      mono: ["Iosevka Curly Iaso", "monospace"],
+      serif: ["Iosevka Etoile Iaso", "serif"],
+    }
+},
+  },
+  plugins: [],
+}
+
