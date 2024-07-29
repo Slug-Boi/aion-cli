3ef99d7c43f715dfb3d2a58bb8e8c72e3d43071a
Theis <thph@itu.dk>
2024-07-29 15:51:57 +0200

    refactor: add testing paramater

--- src/lib_funcs/lib_funcs.go
+++ src/lib_funcs/lib_funcs.go
@@ -8,12 +8,29 @@ import (
 var Sugar = logger.SetupLogger()
 
 // This function will get the config file and setup the config struct
-func SetupConfig(args []string) forms.Config {
+func SetupConfig(args []string, testing ...string) forms.Config {
 	var conf forms.Config
 	var err error
 
-	println(len(args))
+	if len(testing) > 0 {
+		// override formID from config file if formID is provided as an argument
+		if len(args) == 1 {
+			conf, err = forms.GetConfigFile(testing[0])
+			if err != nil {
+				Sugar.Panicf("Error getting the config file using provided formID:\n", err.Error())
+			}
+			conf.FormID = args[0]
+			return conf
+		}
+		// get config file
+		conf, err = forms.GetConfigFile(testing[0])
+		if err != nil {
+			Sugar.Panicf("Error getting the config file:\n", err.Error())
+		}
+		return conf
+	}
 
+	// if formID is provided as an argument
 	if len(args) == 1 {
 		// override formID from config file if formID is provided as an argument
 		conf, err = forms.GetConfigFile()
@@ -21,13 +38,14 @@ func SetupConfig(args []string) forms.Config {
 			Sugar.Panicf("Error getting the config file using provided formID:\n", err.Error())
 		}
 		conf.FormID = args[0]
-	} else {
-		// get config file
-		conf, err = forms.GetConfigFile()
-		if err != nil {
-			Sugar.Panicf("Error getting the config file:\n", err.Error())
-		}
+		return conf
 	}
 
+	// get config file
+	conf, err = forms.GetConfigFile()
+	if err != nil {
+		Sugar.Panicf("Error getting the config file:\n", err.Error())
+	}
 	return conf
+
 }
